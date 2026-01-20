fd6286537d02c9f3e5874aeccd074182a21cbd09
Slupon <sergi.luponsantacana@gmail.com>
2026-01-20 23:19:46 +0100

    eliminate container data

--- dockerfiles/data_remote_entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-cd /app
-
-dvc pull data/raw.dvc
-python -m mlops_g116.data --raw-dir data/raw/brain_dataset --processed-dir data/processed
-dvc add data/processed
-dvc push
