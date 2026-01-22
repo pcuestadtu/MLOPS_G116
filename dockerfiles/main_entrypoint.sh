db28769581baa9558ebf46788a3446ced86364d9
Slupon <sergi.luponsantacana@gmail.com>
2026-01-22 22:11:16 +0100

    fix error for training

--- dockerfiles/main_entrypoint.sh
+++ dockerfiles/main_entrypoint.sh
@@ -30,4 +30,4 @@ extra_args+=("hydra.run.dir=${hydra_run_dir}")
 RUN_SNAKEVIZ="${RUN_SNAKEVIZ:-0}" \
 RUN_TENSORBOARD="${RUN_TENSORBOARD:-0}" \
 OPEN_TENSORBOARD="${OPEN_TENSORBOARD:-0}" \
-exec python -u src/mlops_g116/main.py "${extra_args[@]}" "$@"
+exec python3 -u src/mlops_g116/main.py "${extra_args[@]}" "$@"
